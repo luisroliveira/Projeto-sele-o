9c5e26de13f1366f6f2c593798d0c7507b8d0275
luisfoliveira_ <102266859+luisroliveira@users.noreply.github.com>
2022-08-01 21:04:33 -0300

    Delete Programa UFPE directory

--- Programa UFPE/aNode/Basico Node/http.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-  res.end("Ola pessoal! Bem vindo ao meu website");
-}).listen(8081);
-
-console.log('O servidor está logando');
